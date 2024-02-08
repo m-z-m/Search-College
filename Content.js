727a61ce957789ab2a521d240cb847bcce1c0bba
m-z-m <139612494+m-z-m@users.noreply.github.com>
2024-02-08 14:08:34 +0530

    Create Content.js

---
+++ Content.js
@@ -0,0 +1,97 @@
+const productData = [
+    {
+      id: 1,
+      name: "Sanskriti University ",
+      image:
+      "https://media.collegedekho.com/media/img/institute/logo/2019-04-01_3.jpg?height=150&width=150",
+      productType: "openBackHeadphones",
+      price: "Mathura ",
+      year:"Govt.",
+      rating: 48,
+      timeLeft: "UGC",
+      totalSales: 289,
+      total:1,
+
+    },
+    {
+      id: 2,
+      name: "IIT Roorkee",
+      image:
+      "https://media.collegedekho.com/media/img/institute/logo/1421942233.jpg?height=150&width=150",
+      productType: "openBackHeadphones",
+      price: "Roorkee",
+      year:"Govt.",
+
+      rating: 288,
+      timeLeft: "AICTE NAAC",
+      totalSales: 19,
+      total:1,
+
+    },
+    {
+      id: 3,
+      name: "MNNIT Allahabad",
+      image:
+      "https://media.collegedekho.com/media/img/institute/logo/download_1.jpg?height=150&width=150",
+      productType: "openBackHeadphones",
+      price: "Allahabad",
+      year:"Govt",
+
+      rating: 400,
+      timeLeft: "AICTE",
+      totalSales: 47,  
+      total:1,
+  
+
+    }, 
+    {
+      id: 4,
+      name: "IIT Kanpur",
+      image:
+      "https://media.collegedekho.com/media/img/institute/logo/1479966276logo_104x104.jpeg?height=150&width=150",
+      productType: "openBackHeadphones",
+      price: "Allahabad",
+      year:"Pri.",
+
+      rating: 400,
+      timeLeft: "Oracle ",
+      totalSales: 47,  
+      total:1,
+  
+
+    },
+    {
+      id: 5,
+      name: "St. Stephen College ",
+      image:
+      "https://media.collegedekho.com/media/img/institute/logo/st_stepen.jpg?height=150&width=150",
+      productType: "openBackHeadphones",
+      price: "Delhi",
+      year:"Pri.",
+
+      rating: 400,
+      timeLeft: "UGC ",
+      totalSales: 11,  
+      total:1,
+  
+
+    },
+    {
+      id: 6,
+      name: "DIT University",
+      image:
+      "https://media.collegedekho.com/media/img/institute/logo/DIT_University_Dehradun_Logo_104x104.jpg?height=150&width=150",
+      productType: "openBackHeadphones",
+      price: "Dehradun",
+
+      rating: 400,
+      timeLeft: "UGC AICTE ",
+      totalSales: 179,  
+      total:1,
+  
+
+    } 
+     
+  
+  ];
+  export default productData;
